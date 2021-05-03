37e72753027831fff55319c9cecc483e67be26d4
Juliano Resende <juliano.resende@gmail.com>
2021-05-02 23:05:31 -0300

    finished hangman project, started meal finder

--- hangman/script.js
+++ hangman/script.js
@@ -35,4 +35,71 @@ function displayWord(){
     }
 }
 
+// Update the wrong letters
+function updateWrongLettersEl(){
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+
+    figureParts.forEach((part,index) =>{
+        const errors = wrongLetters.length;
+
+        if(index < errors){
+            part.style.display = 'block';
+        }else{
+            part.style.display = 'none';
+        }
+    });
+
+    // Check if lost
+    if(wrongLetters.length === figureParts.length){
+        finalMessage.innerText = 'Unfortunaley you lost. :-('
+        popup.style.display = 'flex';
+    }
+}
+
+// Show notificiation
+function showNotification(){
+    notification.classList.add('show')
+    setTimeout(() =>{
+        notification.classList.remove('show');
+    }, 2000);
+}
+
+// Keydown letter press
+window.addEventListener('keydown', e =>{
+    if (e.keyCode >= 65 && e.keyCode <= 90){
+        const letter = e.key;
+        if (selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)){
+                correctLetters.push(letter);
+                displayWord();
+            }else{
+                showNotification();
+            }
+        }else{
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter);
+                updateWrongLettersEl();
+            }else{
+                showNotification();
+            }
+        }
+    }
+});
+
+// Restart Game and Play Againt
+playAgainBtn.addEventListener('click', () =>{
+    // Empty arrays
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = words[Math.floor(Math.random() * words.length)]
+    displayWord();
+    updateWrongLettersEl();
+    popup.style.display = 'none';
+})
+
+
 displayWord()
